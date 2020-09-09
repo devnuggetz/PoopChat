489e505327127f3a45c42d07752b427feb5c739b
shuvamk <kshuvam564@gmail.com>
2020-09-08 23:13:17 -0700

    Created React App and Cleaned Up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCKs6AmyrULkr1MkAlVQ8QSLlCJ-7ldEDA",
+    authDomain: "poopchat-3a096.firebaseapp.com",
+    databaseURL: "https://poopchat-3a096.firebaseio.com",
+    projectId: "poopchat-3a096",
+    storageBucket: "poopchat-3a096.appspot.com",
+    messagingSenderId: "280651147103",
+    appId: "1:280651147103:web:ab41a5772b4f0b97fbf8ea",
+    measurementId: "G-M5VBXDV9MS"
+  };
