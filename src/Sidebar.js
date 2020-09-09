ada002e0a59ace803363bc7c8d78f54f6da38c1a
shuvamk <kshuvam564@gmail.com>
2020-09-09 13:06:04 -0700

    Completed Search Bar and Side Header

---
+++ src/Sidebar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Sidebar.css'
+import {Avatar, IconButton} from '@material-ui/core'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import SidebarChats from './SidebarChats';
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <div className='sidebar__header'>
+                <Avatar />
+                <div className='sidebar__headerRight'>
+                <IconButton>
+                <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                <ChatIcon />
+                </IconButton>
+                <IconButton>
+                <MoreVertIcon />
+                </IconButton>
+                 
+                </div>
+            </div>
+            <div className='sidebar__search'>
+                <div className='sidebar__searchContainer'>
+                <SearchOutlinedIcon />
+                <input placeholder='Search' type='text'>
+
+                </input>
+                </div>
+                
+            </div>
+            <div className='sidebar__chats'>
+                <SidebarChats/>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/SidebarChats.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SidebarChats.css'
+
+function SidebarChats() {
+    return (
+        <div className='sidebarChats'>
+            
+        </div>
+    )
+}
+
+export default SidebarChats
